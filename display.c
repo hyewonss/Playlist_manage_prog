92bf20a6d0094396b983558993fac2f8fbf56496
hyewonss <shwshw2003@naver.com>
2022-12-11 00:44:16 +0900

    c

--- display.c
+++ display.c
@@ -2,15 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int n;
-
-int songnum(int j){
-    return j;
-}
 
 void display(node *head){
-    int(*pnum)(int n);
-    pnum=songnum;
     int i=1;
     node *p=head;
     while (p!=NULL){
@@ -18,8 +11,6 @@ void display(node *head){
         printf("가수:%s\n", (p->data).artist);
         printf("곡:%s\n\n", (p->data).song);
         p=p->next;
-        n=i;
         i++;
     }
-    printf("총 %d곡 입니다.\n", pnum(n));
 }
